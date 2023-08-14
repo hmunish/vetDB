1cef968accfab1df28575bc51982380a38cd6785
Munish Halai <hmunish5@gmail.com>
2023-08-14 16:23:52 +0200

    Update name data type in the table

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals(
+CREATE TABLE animals (
     id integer NOT NULL,
-    name character varying(256)[] NOT NULL,
+    name character varying(256) NOT NULL,
     date_of_birth date NOT NULL,
     escape_attempts integer NOT NULL,
     neutered boolean NOT NULL,
