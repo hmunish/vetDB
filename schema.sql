f0a6e6997539cd94215eff86a5f5e9e31bb8eb3c
Munish Halai <hmunish5@gmail.com>
2023-08-16 14:39:53 +0200

    Add owners & species table & define relations

--- schema.sql
+++ schema.sql
@@ -12,3 +12,38 @@ CREATE TABLE animals (
 
 ALTER TABLE IF EXISTS animals ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 5 );
 ALTER TABLE IF EXISTS animals ADD COLUMN species character varying(256);
+
+/*Create owners table*/
+
+CREATE TABLE owners (
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
+    full_name character varying(256) NOT NULL,
+    age integer NOT NULL,
+    PRIMARY KEY (id)
+);
+
+/*Create species table*/
+
+CREATE TABLE species (
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name character varying NOT NULL,
+    PRIMARY KEY (id)
+);
+
+/*DELETE species column from animals table*/
+ALTER TABLE animals DROP COLUMN species;
+
+/*Add Foreign key*/
+ALTER TABLE IF EXISTS animals
+    ADD CONSTRAINT species_id FOREIGN KEY (id)
+    REFERENCES species (id) MATCH SIMPLE
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+    NOT VALID;
+
+ALTER TABLE IF EXISTS animals
+    ADD CONSTRAINT customer_id FOREIGN KEY (id)
+    REFERENCES owners (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID;
