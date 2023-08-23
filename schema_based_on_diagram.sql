ee97d153a9d6af589e2e469ea252a2e645bbd9c1
rabayarabu <rabu9149@gmail.com>
2023-08-23 22:16:57 +0600

    Include Indexes

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -6,12 +6,7 @@ date_of_birth DATE,
 PRIMARY KEY(id)
 );
 
-CREATE TABLE medical_histories(id INT GENERATED ALWAYS AS IDENTITY,
-patient_id INT REFERENCES patients(id),
-admited_at TIMESTAMP,
-status VARCHAR,
-PRIMARY KEY(id)
-);
+
 
 CREATE TABLE treatments(id INT GENERATED ALWAYS AS IDENTITY,
 name VARCHAR,
@@ -42,3 +37,24 @@ medical_histories_id INT REFERENCES medical_histories(id),
 treatment_id INT REFERENCES treatments(id),
 PRIMARY KEY(id);
 );
+
+CREATE TABLE medical_histories(id INT GENERATED ALWAYS AS IDENTITY,
+patient_id INT REFERENCES patients(id),
+admited_at TIMESTAMP,
+status VARCHAR,
+PRIMARY KEY(id)
+);
+
+ALTER TABLE medical_histories ADD CONSTRAINT fk_patients FOREIGN KEY (patient_id) REFERENCES patients(id);
+ALTER TABLE invoices ADD CONSTRAINT fk_medical_histories FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id);
+ALTER TABLE invoice_items ADD CONSTRAINT fk_treatments FOREIGN KEY (treatment_id) REFERENCES treatments(id);
+ALTER TABLE invoice_items ADD CONSTRAINT fk_invoices FOREIGN KEY (invoice_id) REFERENCES invoices(id);
+ALTER TABLE medical_histories_treatments ADD CONSTRAINT fk_treatments FOREIGN KEY (treatment_id) REFERENCES treatments(id);
+ALTER TABLE medical_histories_treatments ADD CONSTRAINT fk_histories FOREIGN KEY (medical_histories_id) REFERENCES medical_histories(id);
+
+CREATE INDEX idx_patient_id ON medical_histories(patient_id);
+CREATE INDEX idx_medical_history_id ON invoices(medical_history_id);
+CREATE INDEX idx_invoice_id ON invoice_items(invoice_id);
+CREATE INDEX idx_treatment_id ON invoice_items(treatment_id);
+CREATE INDEX idx_medical_histories_id ON medical_histories_treatments(medical_histories_id);
+CREATE INDEX idx_treatments_id ON medical_histories_treatments(treatment_id);
